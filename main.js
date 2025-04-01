afa19af840b1287c9f48dc95187f90cbad773239
shouechi <hajimete.1105@gmail.com>
2025-04-01 23:44:37 +0900

    変数、定数の学習

---
+++ main.js
@@ -0,0 +1,11 @@
+let conut = 0;
+conut = 30;
+let newCount = 0;
+const daysInWeek = 7;
+let tomatoConut; //いい書き方
+let TomatoCount; //悪い書き方
+let tomato_conut; //悪い書き方
+let $tomato$Conut; //ドルマーク使用可能
+let _tomato_Conut; //アンダースコア使用可能
+let tomatoConut7; //数字から始まる変数名は使用できない
+let トマトカウント; //日本語の変数名も使用可能
