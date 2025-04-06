a8755a8ea47e2fc0e0a0626560a46b9311ac7778
shouechi <hajimete.1105@gmail.com>
2025-04-07 00:12:11 +0900

    型の学習

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ let $tomato$Conut; //ドルマーク使用可能
 let _tomato_Conut; //アンダースコア使用可能
 let tomatoConut7; //数字から始まる変数名は使用できない
 let トマトカウント; //日本語の変数名も使用可能
+
+let additionResult = 2 + 5;
+let result = 5;
+result = result + 1;
+result += 1;
+result++; //前の値を返す
+++result; //後の値を返す
+let number = 10; //interger型
+number = -4;
+number = 32.42; //folat型
+let string = 'Hello'; //string型
+const userName = 'Yoshipi';
+string = 'Hello' + userName + '!'; //文字列の結合
+string = `Hello ${userName}`; //テンプレートリテラル
+string = `\`\\`;
+// 改行の仕方
+string ='Hello\nYoshipi!'; // backslashを使う
+string = `Hello Yoshipi!`; // テンプレートリテラルを使う
+console.log(string);
